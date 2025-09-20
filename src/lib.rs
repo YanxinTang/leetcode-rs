1cadc16ebb04acb454a53f449c646438baa114ec
tyx1703 <tyx1703@gmail.com>
2025-09-20 15:03:04 +0800

    p1742_maximum_number_of_balls_in_a_box

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+#![allow(dead_code)]
+mod p5_longest_palindromic_substring;
+mod p1742_maximum_number_of_balls_in_a_box;
---
+++ src/p1742_maximum_number_of_balls_in_a_box.rs
@@ -0,0 +1,32 @@
+
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+  pub fn count_balls(low_limit: i32, high_limit: i32) -> i32 {
+    let mut map:HashMap<i32, i32> = HashMap::new();
+      for i in low_limit..=high_limit {
+        let mut num = i;
+        let mut sum = 0;
+        while num > 0 {
+          sum += num % 10;
+          num /= 10;
+        }
+        map.entry(sum).and_modify(|count|{ *count += 1}).or_insert(1);
+      }
+      map.values().max().cloned().unwrap_or(0)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test() {
+    assert_eq!(Solution::count_balls(1, 10), 2);
+    assert_eq!(Solution::count_balls(5, 15), 2);
+    assert_eq!(Solution::count_balls(19, 28), 2);
+  }
+}
